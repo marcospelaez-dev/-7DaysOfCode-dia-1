82a8fc79938cca76a7b1b5f4499d941203609645
Marcos Pelaez / Dev Front End <64918947+marcospelaez-dev@users.noreply.github.com>
2024-04-08 10:56:34 -0300

    Create desafio-1.js

---
+++ desafio-1.js
@@ -0,0 +1,26 @@
+
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+if (numeroUm === stringUm) {
+    console.log('As variáveis numeroUm e stringUm tem o mesmo valor e tipos')  
+  } else {
+    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+  }
+  
+  if (numeroTrinta === stringTrinta) {
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e tipos')  
+  } else {
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor, mas tipos diferentes')
+  }
+  
+  if (numeroDez === stringDez) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor e tipos')
+  } else {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes.')
+  }
+  
